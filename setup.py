f2351cbef2d4f4a95ef91d19f788c4f8b3c8ee1f
sandeepkrpalit <sandeepkrpalit@gmail.com>
2025-01-04 22:27:28 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical-Assist-Chatbot',
+    version= '0.0.0',
+    author= 'Sandeep Kumar Palit',
+    author_email= 'sandeep.palit047@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
